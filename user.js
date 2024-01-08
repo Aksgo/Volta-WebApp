dffd5f56a515c92b57d0a5a15b7c6f2004a54c91
atg2431 <126277528+atg2431@users.noreply.github.com>
2024-01-08 13:15:04 +0530

    Chat Interface and Contacts added

---
+++ user.js
@@ -0,0 +1,15 @@
+
+function openModal(){
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal(){
+    document.getElementById("myModal").style.display = "none";
+}
+
+window.onclick = function(event){
+    var modal = document.getElementById("myModal");
+    if (event.target == modal){
+        closeModal();
+    }
+};
